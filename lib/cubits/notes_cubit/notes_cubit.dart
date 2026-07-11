d748bd86573ed8eb3f4bc8ee5f6707c2befcca27
lanara29 <lana.rashwani.2003@gmail.com>
2026-07-11 14:08:09 +0300

    display notes part one

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -1,18 +1,16 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive/hive.dart';
 import 'package:notes_app/constants.dart';
-import 'package:notes_app/cubits/notes_cubit/notes_state.dart';
+
 import 'package:notes_app/models/note_model.dart';
+part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
-  fetchAllNotes() async {
-    try {
-      var notesBox = Hive.box<NoteModel>(kNotesBox);
-      List<NoteModel> notes = notesBox.values.toList();
-      emit(NotesSuccess(notes: notes));
-    } catch (e) {
-      emit(NotesError(errMessage: e.toString()));
-    }
+  List<NoteModel>? notes;
+  fetchAllNotes() {
+    var notesBox = Hive.box<NoteModel>(kNotesBox);
+    notes = notesBox.values.toList();
   }
 }
--- lib/cubits/notes_cubit/notes_state.dart
+++ lib/cubits/notes_cubit/notes_state.dart
@@ -1,19 +1,6 @@
-import 'package:notes_app/models/note_model.dart';
+part of 'notes_cubit.dart';
 
+@immutable
 abstract class NotesState {}
 
 class NotesInitial extends NotesState {}
-
-class NotesLoading extends NotesState {}
-
-class NotesSuccess extends NotesState {
-  final List<NoteModel> notes;
-
-  NotesSuccess({required this.notes});
-}
-
-class NotesError extends NotesState {
-  final String errMessage;
-
-  NotesError({required this.errMessage});
-}
